1d6baa57cf9d9812c13956c98692d8c54b91fd64
bauuuu1021 <bboy010554@icloud.com>
2020-10-14 03:03:13 +0800

    Remove leading spaces of commands

--- shell.cpp
+++ shell.cpp
@@ -42,14 +42,28 @@ int main () {
 
     setenv("PATH", "bin:.", !0);
 
-    string cmd;
+    string cmd, delimiter = " ";
     cout << "% ";
     while (getline(cin, cmd) && cmd.compare("exit")) {
+
+        // Skip leading space(s) of cmd
+        while (cmd.length()!=0 && cmd.at(0)==' ') {
+            try {
+                cmd = cmd.substr(cmd.find(delimiter)+1);
+            }
+            catch (const std::exception& e) {
+                cerr << "[rm leading space] error:\n" << e.what() << "\n";
+            }
+        }         
+        
         if (!cmd.compare(0, 8, "printenv")) {
             printenv(cmd);
         }
         else if (!cmd.compare(0, 6, "setenv")) {
             setenv(cmd);
+        }
+        else if (cmd.length()==0) {     // empty command
+            ;
         }
         else {
             execute_cmd(cmd.c_str());
