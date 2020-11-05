9e3d46f0e7c1590e74134fbcc073eb70a30c8560
bauuuu1021 <j851021@gmail.com>
2020-11-05 14:35:16 +0800

    Refine src code
    asoiddj

--- task2.cpp
+++ task2.cpp
@@ -11,12 +11,16 @@
 #include <sys/types.h>
 #include <errno.h>
 #include <sys/time.h>
+#include <algorithm>
 
 #define MAX_CLIENTS 30
 #define star_divider "****************************************\n"
 #define welcome_msg star_divider "** Welcome to the information server. **\n" star_divider
 
 using namespace std;
+
+fd_set readfds;
+int client_fd[MAX_CLIENTS] = {0};
 
 extern void shell();
 
@@ -47,6 +51,68 @@ int socket_setup(int port) {
     return server_fd;
 }
 
+void new_connection(int server) {
+
+    struct sockaddr_in address;
+    int addrlen = sizeof(address), new_socket;
+
+    if (FD_ISSET(server, &readfds)) { 
+        if ((new_socket = accept(server, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) { 
+            perror("accept"); 
+            exit(EXIT_FAILURE); 
+        } 
+
+        //inform user of socket number - used in send and receive commands 
+        printf("New connection , socket fd is %d , ip is : %s , port : %d\n" , new_socket , inet_ntoa(address.sin_addr) , ntohs 
+                (address.sin_port)); 
+
+        // Send new connection greeting message 
+        if (send(new_socket, welcome_msg, strlen(welcome_msg), 0) != strlen(welcome_msg)) { 
+            perror("send"); 
+        } 
+
+        // Add new socket to array of sockets 
+        for (int i = 0; i < MAX_CLIENTS; i++) { 
+            if (!client_fd[i]) { // the slot is empty
+                client_fd[i] = new_socket;
+
+                break; 
+            } 
+        } 
+    }
+}
+
+void client_query() {
+
+    struct sockaddr_in address;
+    int valread, addrlen = sizeof(address);  		
+    char buffer[1024];
+
+    for (int i = 0; i < MAX_CLIENTS; i++) {
+
+        if (FD_ISSET(client_fd[i], &readfds)) { 
+
+            // The socket is closed 
+            if ((valread = read(client_fd[i], buffer, 1024)) == 0) {  
+                getpeername(client_fd[i], (struct sockaddr*)&address, (socklen_t*)&addrlen); 
+                printf("Host disconnected , ip %s , port %d \n" , 
+                        inet_ntoa(address.sin_addr), ntohs(address.sin_port)); 
+
+                close(client_fd[i]); 
+                client_fd[i] = 0; // Mark as 0 to reuse
+            } 
+
+            //Echo back the message that came in 
+            else { 
+                //set the string terminating NULL byte on the end 
+                //of the data read 
+                buffer[valread] = '\0'; 
+                send(client_fd[i], buffer, strlen(buffer), 0); 
+            } 
+        } 
+    }
+}
+
 int main(int argc , char **argv) {
 
     if (argc < 2) {
@@ -54,98 +120,30 @@ int main(int argc , char **argv) {
         exit(EXIT_FAILURE);
     }
 
-    struct sockaddr_in address;
-    int new_socket , client_fd[MAX_CLIENTS] = {0}, 
-        activity, valread , sd, max_sd, addrlen = sizeof(address);  		
-    char buffer[1024];
-    fd_set readfds; 
-
+    int activity, max_sd;
     int server_fd = socket_setup(atoi(argv[1]));
 
     while (true) { 
+
         FD_ZERO(&readfds);
         FD_SET(server_fd, &readfds); 
         max_sd = server_fd; 
 
-        //add child sockets to set 
-        for (int i = 0 ; i < MAX_CLIENTS; i++) { 
-            //socket descriptor 
-            sd = client_fd[i]; 
-
-            //if valid socket descriptor then add to read list 
-            if (sd > 0) 
-                FD_SET( sd , &readfds); 
-
-            //highest file descriptor number, need it for the select function 
-            if (sd > max_sd) 
-                max_sd = sd; 
+        // Add client sockets into read list
+        for (int i = 0 ; i < MAX_CLIENTS; i++) {           
+            if (client_fd[i] > 0)
+                FD_SET(client_fd[i], &readfds);
+            max_sd = max(client_fd[i], max_sd); 
         } 
 
-        //wait for an activity on one of the sockets , timeout is NULL , 
-        //so wait indefinitely 
         activity = select(max_sd + 1, &readfds, NULL, NULL, NULL); 
-
         if ((activity < 0) && (errno != EINTR)) { 
             printf("select error"); 
         } 
 
-        //If something happened on the master socket , 
-        //then its an incoming connection 
-        if (FD_ISSET(server_fd, &readfds)) { 
-            if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0) { 
-                perror("accept"); 
-                exit(EXIT_FAILURE); 
-            } 
+        new_connection(server_fd);
 
-            //inform user of socket number - used in send and receive commands 
-            printf("New connection , socket fd is %d , ip is : %s , port : %d\n" , new_socket , inet_ntoa(address.sin_addr) , ntohs 
-                    (address.sin_port)); 
+        client_query();
 
-            //send new connection greeting message 
-            if (send(new_socket, welcome_msg, strlen(welcome_msg), 0) != strlen(welcome_msg)) { 
-                perror("send"); 
-            } 
-
-            puts("Welcome message sent successfully"); 
-
-            //add new socket to array of sockets 
-            for (int i = 0; i < MAX_CLIENTS; i++) { 
-                //if position is empty 
-                if (!client_fd[i]) { 
-                    client_fd[i] = new_socket; 
-                    printf("Adding to list of sockets as %d\n", i); 
-
-                    break; 
-                } 
-            } 
-        } 
-
-        //else its some IO operation on some other socket 
-        for (int i = 0; i < MAX_CLIENTS; i++) { 
-            sd = client_fd[i]; 
-
-            if (FD_ISSET(sd, &readfds)) { 
-                //Check if it was for closing , and also read the 
-                //incoming message 
-                if ((valread = read( sd , buffer, 1024)) == 0) { 
-                    //Somebody disconnected , get his details and print 
-                    getpeername(sd, (struct sockaddr*)&address, (socklen_t*)&addrlen); 
-                    printf("Host disconnected , ip %s , port %d \n" , 
-                            inet_ntoa(address.sin_addr), ntohs(address.sin_port)); 
-
-                    //Close the socket and mark as 0 in list for reuse 
-                    close(sd); 
-                    client_fd[i] = 0; 
-                } 
-
-                //Echo back the message that came in 
-                else { 
-                    //set the string terminating NULL byte on the end 
-                    //of the data read 
-                    buffer[valread] = '\0'; 
-                    send(sd, buffer, strlen(buffer), 0); 
-                } 
-            } 
-        } 
     }
 } 
