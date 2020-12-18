098afd260b5246ab3929629fe8e8f6e14e25e5cf
cabau <cabau@nplinux1.cc.cs.nctu.edu.tw>
2020-12-18 20:43:01 +0800

    Add partial connection informationy

---
+++ Makefile
@@ -0,0 +1,18 @@
+CXX = g++
+CXXFLAGS = -std=c++14 -Wall -pedantic -pthread -lboost_system
+CXX_INCLUDE_DIRS = /usr/local/include
+CXX_INCLUDE_PARAMS = $(addprefix -I , $(CXX_INCLUDE_DIRS))
+CXX_LIB_DIRS = /usr/local/lib
+CXX_LIB_PARAMS = $(addprefix -L , $(CXX_LIB_DIRS))
+
+EXE = sock_server
+
+all: $(EXE)
+
+sock_server: sock4.cpp
+	$(CXX) -o $@ $^ $(CXX_INCLUDE_PARAMS) $(CXX_LIB_PARAMS) $(CXXFLAGS)
+
+.PHONY: clean
+clean:
+	rm -f $(EXE)
+
--- sock4.cpp
+++ sock4.cpp
@@ -1,101 +1,116 @@
-//
-// sync_client.cpp
-// ~~~~~~~~~~~~~~~
-//
-// Copyright (c) 2003-2008 Christopher M. Kohlhoff (chris at kohlhoff dot com)
-//
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-
+#include <cstdlib>
 #include <iostream>
-#include <iomanip>
-#include <ostream>
-#include <string>
+#include <memory>
+#include <utility>
 #include <boost/asio.hpp>
-#include <boost/array.hpp>
-#include "socks4.hpp"
+#include <boost/lexical_cast.hpp>
 
 using boost::asio::ip::tcp;
+using namespace std;
+
+class session
+  : public std::enable_shared_from_this<session>
+{
+public:
+  session(tcp::socket socket)
+    : socket_(std::move(socket))
+  {
+  }
+
+  void start()
+  {
+    do_read();
+  }
+
+private:
+  void do_read()
+  {
+    auto self(shared_from_this());
+    socket_.async_read_some(boost::asio::buffer(data_, max_length),
+        [this, self](boost::system::error_code ec, std::size_t length)
+        {
+          if (!ec)
+          {
+//	    cout << data_ << endl;
+            //do_write(length);
+          }
+        });
+  }
+
+  void do_write(std::size_t length)
+  {
+    auto self(shared_from_this());
+    boost::asio::async_write(socket_, boost::asio::buffer(data_, length),
+        [this, self](boost::system::error_code ec, std::size_t /*length*/)
+        {
+          if (!ec)
+          {
+            do_read();
+          }
+        });
+  }
+
+  tcp::socket socket_;
+  enum { max_length = 1024 };
+  char data_[max_length];
+};
+
+class server
+{
+public:
+  server(boost::asio::io_context& io_context, short port)
+    : acceptor_(io_context, tcp::endpoint(tcp::v4(), port))
+  {
+    do_accept();
+  }
+
+
+private:
+  void do_accept()
+  {
+    acceptor_.async_accept(
+        [this](boost::system::error_code ec, tcp::socket socket)
+        {
+          if (!ec)
+          {
+	    cout << "<S_IP>: " << boost::lexical_cast<std::string>(socket.remote_endpoint().address()) << endl;
+	    cout << "<S_PORT>: " << boost::lexical_cast<std::string>(socket.remote_endpoint().port()) << endl;
+            cout << "<D_IP>: " << endl;
+            cout << "<D_PORT>: " << endl;
+            cout << "<Command>: " << endl;
+            cout << "<Reply>: " << endl;
+	    cout << "----------------------------\n";
+	    std::make_shared<session>(std::move(socket))->start();
+          }
+
+          do_accept();
+        });
+  }
+
+  tcp::acceptor acceptor_;
+ 
+
+};
 
 int main(int argc, char* argv[])
 {
   try
   {
-    if (argc != 4)
+    if (argc != 2)
     {
-      std::cout << "Usage: sync_client <socks4server> <socks4port> <user>\n";
-      std::cout << "Examples:\n";
-      std::cout << "  sync_client 127.0.0.1 1080 chris\n";
-      std::cout << "  sync_client localhost socks chris\n";
+      std::cerr << "Usage: " << argv[0] << " " << "<port>\n";
       return 1;
     }
 
-    boost::asio::io_service io_service;
+    boost::asio::io_context io_context;
 
-    // Get a list of endpoints corresponding to the SOCKS 4 server name.
-    tcp::resolver resolver(io_service);
-    tcp::resolver::query socks_query(argv[1], argv[2]);
-    tcp::resolver::iterator endpoint_iterator = resolver.resolve(socks_query);
-    tcp::resolver::iterator end;
+    server s(io_context, std::atoi(argv[1]));
 
-    // Try each endpoint until we successfully establish a connection to the
-    // SOCKS 4 server.
-    tcp::socket socket(io_service);
-    boost::system::error_code error = boost::asio::error::host_not_found;
-    while (error && endpoint_iterator != end)
-    {
-      socket.close();
-      socket.connect(*endpoint_iterator++, error);
-    }
-    if (error)
-      throw boost::system::system_error(error);
-
-    // Get an endpoint for the Boost website. This will be passed to the SOCKS
-    // 4 server. Explicitly specify IPv4 since SOCKS 4 does not support IPv6.
-    tcp::resolver::query http_query(tcp::v4(), "www.boost.org", "http");
-    tcp::endpoint http_endpoint = *resolver.resolve(http_query);
-
-    // Send the request to the SOCKS 4 server.
-    socks4::request socks_request(
-        socks4::request::connect, http_endpoint, argv[3]);
-    boost::asio::write(socket, socks_request.buffers());
-
-    // Receive a response from the SOCKS 4 server.
-    socks4::reply socks_reply;
-    boost::asio::read(socket, socks_reply.buffers());
-
-    // Check whether we successfully negotiated with the SOCKS 4 server.
-    if (!socks_reply.success())
-    {
-      std::cout << "Connection failed.\n";
-      std::cout << "status = 0x" << std::hex << socks_reply.status();
-      return 1;
-    }
-
-    // Form the HTTP request. We specify the "Connection: close" header so that
-    // the server will close the socket after transmitting the response. This
-    // will allow us to treat all data up until the EOF as the response.
-    std::string request =
-      "GET / HTTP/1.0\r\n"
-      "Host: www.boost.org\r\n"
-      "Accept: */*\r\n"
-      "Connection: close\r\n\r\n";
-
-    // Send the HTTP request.
-    boost::asio::write(socket, boost::asio::buffer(request));
-
-    // Read until EOF, writing data to output as we go.
-    boost::array<char, 512> response;
-    while (std::size_t s = socket.read_some(
-          boost::asio::buffer(response), error))
-      std::cout.write(response.data(), s);
-    if (error != boost::asio::error::eof)
-      throw boost::system::system_error(error);
+    io_context.run();
   }
   catch (std::exception& e)
   {
-    std::cout << "Exception: " << e.what() << "\n";
+    std::cerr << "Exception: " << e.what() << "\n";
   }
 
   return 0;
